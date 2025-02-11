92b4d341e6f5939ccca26ff5196c8c277fbac59a
Juan Carlos <juancarlosfilter@gmail.com>
2025-02-11 12:53:33 +0100

    aad y no add

---
+++ XQuery/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+import org.basex.api.client.ClientSession;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            ClientSession session = new ClientSession("localhost", 1984, "admin", "admin");
+            session.execute("OPEN dragonball");
+
+            String query = "for $p in //personaje return <nombre>{$p/nombre/text()}</nombre>";
+            String result = session.execute("XQUERY " + query);
+
+            System.out.println("Resultado de la consulta:");
+            System.out.println(result);
+
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
